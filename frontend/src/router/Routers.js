0b3db1e1d48a4aab22c8d0afd1035a10b23b26a1
Vyomrana02 <vyom.rana02@gmail.com>
2024-03-29 20:51:26 +0530

    LAXY

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -27,39 +27,8 @@ const CommonRoutes = React.lazy(()=>import("./CommonRoutes"));
 const Routers = () => {
    const { user, dispatch } = useContext(AuthContext)
    return (
-      //       <Routes>
-      //          {user == null ? <>
-      // <Route path='/' element={<Navigate to='/home'/>} />
-      // <Route path='/home' element={<Home/>} />
-      // <Route path='/tours' element={<Tours/>} />
-      // <Route path='/tours/:id' element={<TourDetails/>} />
-      // <Route path='/login' element={<Login/>} />
-      // <Route path='/register' element={<Register/>} />
-      // <Route path='/tours/search' element={<SearchResultList/>} />
-      // <Route path='/About' element={<About/>} />
-      // </>
-      //          : (user.roles === 'admin' ? 
-      //          <>
-      //          <Route path='/AddTour' element={<AddTour/>} />
-      //          <Route path='/UpdateTour' element={<UpdateTour/>} />
-      //          <Route path='/AllUser' element={<AllUser/>} />
-      //          <Route path='/bookings' element={<Bookings/>} />
-      //          </>
-      //          :
-      //          <>
-      //          <Route path='/thank-you' element={<ThankYou/>} />
-      //          <Route path='/update-user' element={<UpdateUser/>} />
-      //          <Route path='/forgot-password' element={<ForgotPassword/>} />
-      //          <Route path='/reset-password/:id/:token' element={<ResetPassword/>} />
-      //          </>
-      //          )  }
-      //       </Routes>
-
-      // <BrowserRouter>
       <>
-       {/* <React.Suspense fallback={<BounceLoader color="#faa935"  cssOverride={{
-    textAlign: 'center',position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)"
-  }}/>}>  */}
+       
             {user == null ? <>
                <NoLoginRoutes />
             </>
@@ -76,7 +45,6 @@ const Routers = () => {
                      <CommonRoutes/>
                   </>
                )}
-          {/* </React.Suspense> */}
          </>
    )
 }
