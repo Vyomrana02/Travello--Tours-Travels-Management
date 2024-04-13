ca5f83c1925e4da0326525c70a157aec9bd75947
Vyom Rana <vyom.rana02@gmail.com>
2024-04-13 21:38:00 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,2 +1,2 @@
 //export const BASE_URL = 'http://localhost:4000/api/v1'
-export const BASE_URL = 'https://travello-tours-travels-management.onrender.com/api/v1'
+export const BASE_URL = 'https://travello-tours-travels-management-backend.onrender.com/api/v1'
