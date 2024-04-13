dabab93eaf01148b23b19f613088265fcf0632d0
Vyom Rana <vyom.rana02@gmail.com>
2024-04-13 20:32:39 +0530

    Update config.js

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,3 +1,4 @@
-// export const BASE_URL = 'https://travello-tours-and-travels.onrender.com'
-export const BASE_URL_SERVER = 'http://localhost:4000'
-export const BASE_URL_CLIENT = 'http://localhost:3000'
+export const BASE_URL_CLIENT = 'https://travello-tours-and-travels.onrender.com'
+export const BASE_URL_SERVER = 'https://travello-tours-travels-management-backend.onrender.com'
+//export const BASE_URL_SERVER = 'http://localhost:4000'
+//export const BASE_URL_CLIENT = 'http://localhost:3000'
