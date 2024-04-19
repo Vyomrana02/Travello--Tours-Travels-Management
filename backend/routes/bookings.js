3b7041a575d13e8617719f3cb8be3871657e6996
Vyom Rana <vyom.rana02@gmail.com>
2024-04-19 12:00:01 +0530

    Update bookings.js

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -3,12 +3,11 @@ import { createBooking, getAllBooking, getBooking ,createPayment,checkOverlap, c
 import { verifyAdmin, verifyUser} from '../utils/verifyToken.js'
 
 const router = express.Router()
-// const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 router.post('/', verifyUser, createBooking)
 router.post('/Check', verifyUser, checkOverlap)
 router.post('/payment',verifyUser, createPayment)
-router.get('/:ids',getBooking)
+router.get('/:ids',verifyUser,getBooking)
 router.get('/', verifyAdmin, getAllBooking)
 router.post('/cancel', verifyUser, cancelBooking)
 router.post('/CheckGuestSize',verifyUser,CheckGuestSize);
